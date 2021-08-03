f06732e47dd117e0ef6b3d0c801a16fd30f9c6c0
Suraj Thapa <suraj.thapa@mindbowser.com>
2021-08-03 13:55:49 +0530

    initial commit

---
+++ src/Map.js
@@ -0,0 +1,59 @@
+import * as React from "react";
+import ReactMapGL, { NavigationControl, Marker, Layer } from "react-map-gl";
+import Location from "./location.png";
+
+const MAPBOX_TOKEN =
+  "pk.eyJ1Ijoic3VyYWpmYyIsImEiOiJja3E5NWttYjMwbW82MnVvd29mdmg0aGZ3In0.IqFMjczorMTePeMMqHQfnA"; // Set your mapbox token here
+
+const waterLayer = {
+  id: "water",
+  source: "mapbox-streets",
+  "source-layer": "water",
+  type: "fill",
+  paint: {
+    "fill-color": "#00ffff",
+  },
+};
+
+const Map = () => {
+  const [viewport, setViewport] = React.useState({
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8,
+  });
+  // eslint-disable-next-line
+  const [waterColor, setWaterColor] = React.useState("blue");
+
+  const navControlStyle = {
+    right: 10,
+    bottom: 10,
+  };
+
+  return (
+    <ReactMapGL
+      {...viewport}
+      width="100vw"
+      height="100vh"
+      onViewportChange={setViewport}
+      mapboxApiAccessToken={MAPBOX_TOKEN}
+      mapStyle="mapbox://styles/mapbox/dark-v9"
+    >
+      <Layer {...waterLayer} paint={{ "fill-color": waterColor }} />
+      <NavigationControl
+        style={navControlStyle}
+        showCompass={true}
+        showZoom={true}
+      />
+      <Marker
+        latitude={37.78}
+        longitude={-122.41}
+        offsetLeft={-20}
+        offsetTop={-10}
+      >
+        <img src={Location} alt="pin" height="40px" width="30px" />
+      </Marker>
+    </ReactMapGL>
+  );
+};
+
+export default Map;
